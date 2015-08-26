794dda364bd01646225ce04c104a94b8643c4fa1
Baldur Helgason <baldur.helgason@gmail.com>
2015-08-26 09:46:11 +0000

    Removes logging

--- src/leit.js
+++ src/leit.js
@@ -6,7 +6,6 @@
     price = cleanPrice(strongs.eq(0).text());
     size = cleanSize(strongs.eq(1).text());
     sqPrice = calculateSqPrice(price, size);
-    console.log(sqPrice);
     if (isFinite(sqPrice)) {
       $(this).append('<span>Fermetraverð: <strong>' + formatPrice(sqPrice) + '</strong></span>');
     }
