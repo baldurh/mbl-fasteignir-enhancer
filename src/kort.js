f88f4dc84521e2faf36926fd67756250bc15c4a4
Baldur Helgason <baldur.helgason@gmail.com>
2015-08-27 08:48:42 +0000

    Minor code style changes

--- src/kort.js
+++ src/kort.js
@@ -1,25 +1,27 @@
 (function(){
-var price, size, sqPrice, oldHref = '', modal = document.querySelector('.popupmodal');
-var observer = new MutationObserver(function(mutations){
-    mutations.forEach(function(mutation){
-    	var aEle = jQuery('a.mp-skodaeign')[0];
-    	if(typeof(aEle) === 'undefined') return;
-        var href = jQuery(aEle).attr('href');
+  var price, size, sqPrice, observer, aEle, href, config, oldHref, modal;
+  oldHref = '';
+  modal = $('.popupmodal');
+  observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      aEle = $('a.mp-skodaeign')[0];
+      if(typeof(aEle) === 'undefined') return;
+        href = $(aEle).attr('href');
         if(href === 'undefined') return;
         if(oldHref !== href) {
-        	jQuery('span#fmverd').remove();
-        	oldHref = href;
-    		price = cleanPrice(jQuery('.mp-verd-val').text());
-				size = cleanSize(jQuery('.mp-staerd-val').text());
-				sqPrice = calculateSqPrice(price, size);
-				if (isFinite(sqPrice)) {
-					jQuery('span.mp-field:contains("Stærð:")').after('<span class="mp-field" id="fmverd"><span>m<sup>2</sup></span> verð: <span class="mp-val">'+formatPrice(sqPrice)+'</span><span class="mp-val"></span></span>');
-			}
+          $('span#fmverd').remove();
+          oldHref = href;
+          price = cleanPrice($('.mp-verd-val').text());
+          size = cleanSize($('.mp-staerd-val').text());
+          sqPrice = calculateSqPrice(price, size);
+          if (isFinite(sqPrice)) {
+            $('span.mp-field:contains("Stærð:")').after('<span class="mp-field" id="fmverd"><span>m<sup>2</sup></span> verð: <span class="mp-val">' + formatPrice(sqPrice) + '</span><span class="mp-val"></span></span>');
+          }
         }
-    });    
-});
+    });
+  });
 
-var config = {attributes: true};
-observer.observe(modal, config);
+  config = { attributes: true };
+  observer.observe(modal, config);
 
 })();
